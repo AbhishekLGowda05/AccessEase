ff9ce41207e4cf5d25b8b16b937409c2eaae3821
AbhishekLGowda05 <AbhishekLGowda05@gmail.com>
2024-12-12 21:45:25 +0530

    debug

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 const express = require('express');
-const dotenv=require('dotenv');
-const routes = require('./routes');
-const User= require('./UserModel/Model');
+const dotenv = require('dotenv');
 const path = require('path');
+const cors = require('cors');
+const ConnectDB = require('./Database');
+const userRoutes = require('./routes');
 
-const ConnectDB= require('./Database');
 dotenv.config();
 ConnectDB();
 
 const app = express();
+
 app.use(express.json());
-app.use('/extensions',express.static(path.join(__dirname,'frontend')));
+app.use(cors());
 
-app.use('/extensions',express.static(path.join(__dirname,'Extensions')));
-app.use("/api/users",routes);
+app.use('/extensions', express.static(path.join(__dirname, 'Extensions')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','index.html')
+app.use('/api/users', userRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'), (err) => {
+    if (err) res.status(500).send('Error loading frontend');
+  });
 });
 
-app.listen(5000,()=>{
-
-    console.log('server started listening at port 5000');
-})
+app.listen(5000, () => console.log('Server is running on port 5000'));
